f76acfe33c91cad24058ee9af373a6c59c43e6a3
Jason Clay <anticlergygang@gmail.com>
2017-09-05 00:05:30 -0400

    initswag
    yeyeye you already know what it issss

---
+++ shdb.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const mime = require('mime-types');
+const flattenArray = (arr, result = []) => {
+    for (let i = 0, length = arr.length; i < length; i++) {
+        const value = arr[i];
+        if (Array.isArray(value)) {
+            flattenArray(value, result);
+        } else {
+            result.push(value);
+        }
+    }
+    return result;
+};
+const readdirRecursivePromise = path => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, (err, directoriesPaths) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (directoriesPaths.indexOf('.DS_Store') != -1) {
+                    directoriesPaths.splice(directoriesPaths.indexOf('.DS_Store'), 1);
+                }
+                directoriesPaths.forEach((e, i) => {
+                    directoriesPaths[i] = statPromise(`${path}/${e}`);
+                });
+                Promise.all(directoriesPaths).then(out => {
+                    resolve(flattenArray(out));
+                }).catch(err => {
+                    reject(err);
+                });
+            }
+        });
+    });
+};
+const statPromise = path => {
+    return new Promise((resolve, reject) => {
+        fs.stat(path, (err, stats) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (stats.isDirectory()) {
+                    readdirRecursivePromise(path).then(out => {
+                        resolve(out);
+                    }).catch(err => {
+                        reject(err);
+                    });
+                } else if (stats.isFile()) {
+                    resolve({
+                        'path': path,
+                        'type': mime.lookup(path)
+                    });
+                } else {
+                    reject(`Error parsing path: ${path}`);
+                }
+            }
+        });
+    });
+};
+exports.initDBPromise = (path) => {
+    return new Promise((resolve, reject) => {
+        readdirRecursivePromise(path).then(files => {
+            resolve(files);
+        }).catch(err => {
+            reject(err);
+        });
+    });
+};
