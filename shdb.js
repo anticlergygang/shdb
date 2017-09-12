b25afd5d8742982a1290a3a9e3cf266c1c7dcfbe
Jason Clay <anticlergygang@gmail.com>
2017-09-12 00:11:11 -0400

    nvm

--- shdb.js
+++ shdb.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const mime = require('mime-types');
-Array.prototype.flatten = (arr, result = []) => {
+const flattenArray = (arr, result = []) => {
     for (let i = 0, length = arr.length; i < length; i++) {
         const value = arr[i];
         if (Array.isArray(value)) {
-            Array.flatten(value, result);
+            flattenArray(value, result);
         } else {
             result.push(value);
         }
@@ -24,7 +24,7 @@ const readdirRecursivePromise = path => {
                     directoriesPaths[newPathIndex] = statPromise(`${path}/${newPath}`);
                 });
                 Promise.all(directoriesPaths).then(out => {
-                    resolve(Array.flatten(out));
+                    resolve(flattenArray(out));
                 }).catch(err => {
                     reject(err);
                 });
