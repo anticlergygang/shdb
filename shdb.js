7aa6d95dacc179736fa1595a5fca39d4c129a79d
anticlergygang <anticlergygang@gmail.com>
2018-10-03 21:06:05 -0400

    .enc are belong to me

--- shdb.js
+++ shdb.js
@@ -132,6 +132,8 @@ const decipherdir = (directory, password) => {
                             }
                         })
                     })
+                } else {
+                    count = count + 1
                 }
             })
         }).catch(err => {
