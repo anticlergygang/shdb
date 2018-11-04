9cb3938e9127cd3a423744a8056478689708d80b
anticlergygang <anticlergygang@gmail.com>
2018-11-04 02:32:16 -0500

    bruv

--- shdb.js
+++ shdb.js
@@ -1,6 +1,6 @@
 const mime = require('mime-types')
 const lzma = require('lzma-native')
-
+const fs = require('fs')
 const readDirPromise = path => {
     return new Promise((resolve, reject) => {
         fs.readdir(path, (err, files) => {
