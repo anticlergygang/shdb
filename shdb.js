af17098aa8cbe83b8000cf3ef8d5e8c7334e9a7e
anticlergygang <anticlergygang@gmail.com>
2019-05-03 08:37:18 -0400

    mtime ctime atime btime

--- shdb.js
+++ shdb.js
@@ -25,7 +25,6 @@ const statPromise = path => {
 }
 const updateDatabase = mainPath => {
     return new Promise((resolve, reject) => {
-        let files = []
         readDirPromise(mainPath).then(dirInfo => {
             let statArr = [statPromise(mainPath)]
             dirInfo.files.forEach((path, pathIndex) => {
@@ -36,15 +35,34 @@ const updateDatabase = mainPath => {
             let subDirectories = []
             let readyToRead = true
             statInfo.forEach((stat, statIndex) => {
-                if (stat.stats.isDirectory()) {
-                    exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
-                    subDirectories.push(stat.path)
-                } else if (stat.stats.isFile()) {
-                    if (mime.lookup(stat.path)) {
-                        exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                if (exports.database[stat.path.replace(mainPath, '') || '/']) {
+                    if (exports.database[stat.path.replace(mainPath, '') || '/'].stats.mtimeMs !== stat.stats.mtimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.ctimeMs !== stat.stats.ctimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.atimeMs !== stat.stats.atimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.birthtimeMs !== stat.stats.birthtimeMs) {
+                        if (stat.stats.isDirectory()) {
+                            exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
+                            subDirectories.push(stat.path)
+                        } else if (stat.stats.isFile()) {
+                            if (mime.lookup(stat.path)) {
+                                exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                            } else {
+                                exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                            }
+                        }
+                        console.log(`update: ${stat.path}`)
                     } else {
-                        exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                        console.log(`do nothing to: ${stat.path}`)
                     }
+                } else {
+                    if (stat.stats.isDirectory()) {
+                        exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
+                        subDirectories.push(stat.path)
+                    } else if (stat.stats.isFile()) {
+                        if (mime.lookup(stat.path)) {
+                            exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                        } else {
+                            exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                        }
+                    }
+                    console.log(`update: ${stat.path}`)
                 }
             })
             let readInterval = setInterval(() => {
@@ -58,15 +76,34 @@ const updateDatabase = mainPath => {
                         return Promise.all(statArr)
                     }).then(statInfo => {
                         statInfo.forEach((stat, statIndex) => {
-                            if (stat.stats.isDirectory()) {
-                                exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
-                                subDirectories.push(stat.path)
-                            } else if (stat.stats.isFile()) {
-                                if (mime.lookup(stat.path)) {
-                                    exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                            if (exports.database[stat.path.replace(mainPath, '') || '/']) {
+                                if (exports.database[stat.path.replace(mainPath, '') || '/'].stats.mtimeMs !== stat.stats.mtimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.ctimeMs !== stat.stats.ctimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.atimeMs !== stat.stats.atimeMs || exports.database[stat.path.replace(mainPath, '') || '/'].stats.birthtimeMs !== stat.stats.birthtimeMs) {
+                                    if (stat.stats.isDirectory()) {
+                                        exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
+                                        subDirectories.push(stat.path)
+                                    } else if (stat.stats.isFile()) {
+                                        if (mime.lookup(stat.path)) {
+                                            exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                                        } else {
+                                            exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                                        }
+                                    }
+                                    console.log(`update: ${stat.path}`)
                                 } else {
-                                    exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                                    console.log(`do nothing to: ${stat.path}`)
                                 }
+                            } else {
+                                if (stat.stats.isDirectory()) {
+                                    exports.database[stat.path.replace(mainPath, '') || '/'] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'stats': stat.stats, type: 'directory' }
+                                    subDirectories.push(stat.path)
+                                } else if (stat.stats.isFile()) {
+                                    if (mime.lookup(stat.path)) {
+                                        exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': mime.lookup(stat.path), 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                                    } else {
+                                        exports.database[stat.path.replace(mainPath, '')] = { 'path': stat.path, 'linkPath': stat.path.replace(mainPath, ''), 'type': 'unknown', 'stats': stat.stats, 'data': fs.readFileSync(stat.path) }
+                                    }
+                                }
+                                console.log(`update: ${stat.path}`)
                             }
                         })
                         readyToRead = true
