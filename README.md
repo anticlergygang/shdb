a483395c4a43ba914f61439d541c2b6d5281f10e
antiClergy <anticlergygang@gmail.com>
2020-09-22 17:18:18 -0400

    shdb, folderLock

--- README.md
+++ README.md
@@ -3,11 +3,54 @@
 find a dev here if you need help: https://discord.gg/p46mJWm
 
 ```js
+const syncDirToDatabase = async (dir, filelist = []) => {
+	/*
+		This function returns the string "success" and will also update the shdb.database object with the files found under the directory suppied in the paramter.
+	*/
+}
+
+/*
+
+Warning, the functions below can result in data loss, so be careful when using them. You should take a look at the underlying code and understand it before using these functions.
+
+*/
+
+const lockFile = async (path, password) => {
+	/*
+		This function returns the string "success" and will also lock a file with a password.
+	*/
+}
+const unLockFile = async (path, password) => {
+	/*
+		This function returns the string "success" and will also unlock a file with a password.
+	*/
+}
+const lockDir = async (directoryPath, password, filelist = []) => {
+	/*
+		This function returns the string "success" and will also lock every file in a directory with a password.
+	*/
+}
+const unLockDir = async (directoryPath, password, filelist = []) => {
+	/*
+		This function returns the string "success" and will also unlock every file in a directory with a password.
+	*/
+}
+
+//database example, change '/path/to/database/directory' to some random test folder.
+//    remeber to use absolute paths for everything.
+
 let shdb = require('shdb')
 shdb.syncDirToDatabase('/path/to/database/directory').then(success => { // init db
     console.log(success) // This will just say "finished".
     console.log(shdb.database) // This will output be an object of files found under your data directory inserted.
 }).catch(err => {
     console.log(err)
+})
+
+
+shdb.lockDir('C:\\Users\\jason\\Desktop\\test', 'password').then(success => {
+	console.log(success)                    
+}).catch(err => {
+	console.log(err)
 })
 ```
--- shdb.js
+++ shdb.js
@@ -2,15 +2,13 @@ const crypto = require('crypto')
 const fs = require('fs')
 const fsp = require('fs').promises
 const path = require('path')
-
 let database = {
     files: {}
 }
-
-const syncDirToDatabase = async (dir, filelist = []) => {
-    const files = await fsp.readdir(dir)
+const syncDirToDatabase = async (directoryPath, filelist = []) => {
+    const files = await fsp.readdir(directoryPath)
     for (file of files) {
-        const filepath = path.join(dir, file)
+        const filepath = path.join(directoryPath, file)
         const stat = await fsp.stat(filepath)
         if (stat.isDirectory()) {
             filelist = await syncDirToDatabase(filepath, filelist)
@@ -25,8 +23,104 @@ const syncDirToDatabase = async (dir, filelist = []) => {
             fileHandle.close()
         }
     }
-    return 'finished'
+    return 'success'
 }
-
+crypto.scryptp = (password, salt, length) => {
+    return new Promise((resolve, reject) => {
+        crypto.scrypt(password, salt, length, (err, derivedKey) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(derivedKey)
+            }
+        })
+    })
+}
+const lockFile = async (path, password) => {
+    try {
+        const inputFileHandle = await fsp.open(path, 'r')
+        const inputFileData = await inputFileHandle.readFile()
+        const closeInputFile = await inputFileHandle.close()
+        const fileWriteStream = fs.createWriteStream(`${path}.enc`)
+        const salt = crypto.randomBytes(24)
+        fileWriteStream.write(salt)
+        const key = await crypto.scryptp(password, salt, 24)
+        const iv = crypto.randomBytes(16)
+        fileWriteStream.write(iv)
+        const cipher = crypto.createCipheriv('aes-192-cbc', key, iv)
+        cipher.on('readable', () => {
+            let chunk
+            while (null !== (chunk = cipher.read())) {
+                fileWriteStream.write(chunk)
+            }
+        })
+        cipher.on('end', () => {
+            fileWriteStream.end()
+            fsp.unlink(path)
+        })
+        cipher.write(inputFileData)
+        cipher.end()
+        return 'success'
+    } catch (err) {
+        console.log(err)
+    }
+}
+const unLockFile = async (path, password) => {
+    try {
+        const inputFileHandle = await fsp.open(path, 'r')
+        const inputFileData = await inputFileHandle.readFile()
+        const closeInputFile = await inputFileHandle.close()
+        const fileWriteStream = fs.createWriteStream(path.slice(0, -4))
+        const salt = inputFileData.slice(0, 24)
+        const key = await crypto.scryptp(password, salt, 24)
+        const iv = inputFileData.slice(24, 40)
+        const decipher = crypto.createDecipheriv('aes-192-cbc', key, iv)
+        decipher.on('readable', () => {
+            while (null !== (chunk = decipher.read())) {
+                fileWriteStream.write(chunk)
+            }
+        })
+        decipher.on('end', () => {
+            fileWriteStream.end()
+            fsp.unlink(path)
+        })
+        const encryptedData = inputFileData.slice(40)
+        decipher.write(encryptedData)
+        decipher.end()
+        return 'success'
+    } catch (err) {
+        console.log(err)
+    }
+}
+const lockDir = async (directoryPath, password, filelist = []) => {
+    const files = await fsp.readdir(directoryPath)
+    for (file of files) {
+        const filepath = path.join(directoryPath, file)
+        const stat = await fsp.stat(filepath)
+        if (stat.isDirectory()) {
+            filelist = await lockDir(filepath, password, filelist)
+        } else {
+            lockFile(filepath, password)
+        }
+    }
+    return 'success'
+}
+const unLockDir = async (directoryPath, password, filelist = []) => {
+    const files = await fsp.readdir(directoryPath)
+    for (file of files) {
+        const filepath = path.join(directoryPath, file)
+        const stat = await fsp.stat(filepath)
+        if (stat.isDirectory()) {
+            filelist = await unLockDir(filepath, password, filelist)
+        } else {
+            unLockFile(filepath, password)
+        }
+    }
+    return 'success'
+}
 exports.syncDirToDatabase = syncDirToDatabase
 exports.database = database
+exports.lockFile = lockFile
+exports.unLockFile = unLockFile
+exports.lockDir = lockDir
+exports.unLockDir = unLockDir
